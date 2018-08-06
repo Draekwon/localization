463326030250cc22c4923463db5cd604fdc5b666
sveb36 <sveb36@mi.fu-berlin.de>
2018-08-06 15:12:44 +0200

    stuff

--- catkin_ws_user/src/fisheye_camera_matrix/src/offline_calibration_manual.cpp
+++ catkin_ws_user/src/fisheye_camera_matrix/src/offline_calibration_manual.cpp
@@ -1,147 +1,222 @@
-#include <string>
+#include "opencv2/ccalib/omnidir.hpp"
+#include "opencv2/core.hpp"
+#include "opencv2/imgproc.hpp"
+#include "opencv2/calib3d.hpp"
+#include "opencv2/highgui.hpp"
+#include <vector>
 #include <iostream>
-#include <fstream>
-#include <ros/ros.h>
-#include <ros/package.h>
-#include <opencv2/core.hpp>
-#include <opencv2/ccalib/omnidir.hpp>
-#include <opencv2/highgui/highgui.hpp>
-#include <opencv2/imgproc/imgproc.hpp>
-#include "fisheye_camera_matrix/camera_matrix.hpp"
+#include <string>
+#include <time.h>
 
-cv::Mat img;
-cv::Mat img_ud;
+using namespace cv;
+using namespace std;
 
-// intrinsic
-int width;
-int height;
-int cx;
-int cy;
-
-// extrinsic
-int wz;
-int nZRotation;
-int nYRotation;
-int nXRotation = 180;
-
-
-int fl              = 320;
-int fl_max          = 960;
-int ceil_height     = 200;
-int ceil_height_max = 350;
-int scale           = 10;
-int scale_max       = 30;
-
-int wzMax 			= 200;
-
-cv::Mat camMat;
-cv::Mat distCoeffs;
-std::vector<cv::Mat> rvecs;
-std::vector<cv::Mat> tvecs;
-
-std::vector<cv::Mat> objectPoints;
-
-std::vector<cv::Mat> imagePoints;
-
-
-void apply(int, void*)
+static void calcChessboardCorners(const Size &boardSize, const Size2d &squareSize, Mat& corners)
 {
-  // convert degrees -> radians
-  double dZRotation = nZRotation / 180.0 * M_PI;
-  double dYRotation = dYRotation / 180.0 * M_PI;
-  double dXRotation = dXRotation / 180.0 * M_PI;
-
-  camMat = cv::Mat1d::eye(3, 3);
-  camMat.at<double>(0, 0) = fl;
-  camMat.at<double>(1, 1) = fl;
-  camMat.at<double>(0, 2) = cx;
-  camMat.at<double>(1, 2) = cy;
-
-  cv::Size imageSize(img.cols, img.rows);
-
-  cv::Mat K, xi, D, idx;
-  int flags = 0;
-  cv::TermCriteria critia(cv::TermCriteria::COUNT + cv::TermCriteria::EPS, 200, 0.0001);
-  std::vector<cv::Mat> rvecs, tvecs;
-  std::cout << "((cv::InputArrayOfArrays)objectPoints).type() " << ((cv::InputArrayOfArrays)objectPoints).type() << std::endl;
-  std::cout << "(((6) & ((1 << 3) - 1)) + (((3)-1) << 3))): " << ((((6) & ((1 << 3) - 1)) + (((3)-1) << 3))) << std::endl;
-  std::cout << "before calib " << (!((cv::InputArrayOfArrays)objectPoints).empty() && (((cv::InputArrayOfArrays)objectPoints).type() == CV_64FC3)) << std::endl;
-  CV_Assert(!((cv::InputArrayOfArrays)objectPoints).empty() && (((cv::InputArrayOfArrays)objectPoints).type() == CV_64FC3));
-  std::cout << " after assert " << std::endl;
-  double rms = cv::omnidir::calibrate(objectPoints, imagePoints, imageSize, K, xi, D, rvecs, tvecs, flags, critia, idx);
-  std::cout << "after calib" << std::endl;
-  cv::omnidir::undistortImage(img, img_ud, K, D, xi, cv::omnidir::RECTIFY_PERSPECTIVE);
-
-  std::cout << width << " " << height << " " << cx << " " << cy << " "
-	      << fl << " " << (ceil_height / 100.0) << " " << (scale / 10.0) << std::endl;
-
-  cv::imshow("undistorted", img_ud);
+    // corners has type of CV_64FC3
+    corners.release();
+    int n = boardSize.width * boardSize.height;
+    corners.create(n, 1, CV_64FC3);
+    Vec3d *ptr = corners.ptr<Vec3d>();
+    for (int i = 0; i < boardSize.height; ++i)
+    {
+        for (int j = 0; j < boardSize.width; ++j)
+        {
+            ptr[i*boardSize.width + j] = Vec3d(double(j * squareSize.width), double(i * squareSize.height), 0.0);
+        }
+    }
 }
 
-int main(int argc, char **argv) {
-  ros::init(argc, argv, "calibration");
+static bool detecChessboardCorners(const vector<string>& list, vector<string>& list_detected,
+    vector<Mat>& imagePoints, Size boardSize, Size& imageSize)
+{
+    imagePoints.resize(0);
+    list_detected.resize(0);
+    int n_img = (int)list.size();
+    Mat img;
+    for(int i = 0; i < n_img; ++i)
+    {
+        cout << list[i] << "... ";
+        Mat points;
+        img = imread(list[i], IMREAD_GRAYSCALE);
+        bool found = findChessboardCorners( img, boardSize, points);
+        if (found)
+        {
+            if (points.type() != CV_64FC2)
+                points.convertTo(points, CV_64FC2);
+            imagePoints.push_back(points);
+            list_detected.push_back(list[i]);
+        }
+        cout << (found ? "FOUND" : "NO") << endl;
+    }
+    if (!img.empty())
+        imageSize = img.size();
+    if (imagePoints.size() < 3)
+        return false;
+    else
+        return true;
+}
 
-  std::cout << argc << std::endl;
-  std::cout << argv[0] << " " << argv [argc-1] << std::endl;
+static bool readStringList( const string& filename, vector<string>& l )
+{
+    l.resize(0);
+    FileStorage fs(filename, FileStorage::READ);
+    if( !fs.isOpened() )
+        return false;
+    FileNode n = fs.getFirstTopLevelNode();
+    if( n.type() != FileNode::SEQ )
+        return false;
+    FileNodeIterator it = n.begin(), it_end = n.end();
+    for( ; it != it_end; ++it )
+        l.push_back((string)*it);
+    return true;
+}
 
-  if(argc < 3) {
-    ROS_ERROR("Please use roslaunch: 'roslaunch fisheye_camera_matrix calibrate_offline_manual.launch "
-              "[img:=FILE] [calib:=FILE]'");
-    return 1;
-  }
-  std::cout << ros::package::getPath("fisheye_camera_matrix") << std::endl;
-  std::string path_img   = ros::package::getPath("fisheye_camera_matrix") + std::string("/../../../captures/") + std::string(argv[1]);
-  std::string path_calib = ros::package::getPath("fisheye_camera_matrix") + std::string("/config/") + std::string(argv[2]);
+static void saveCameraParams( const string & filename, int flags, const Mat& cameraMatrix,
+    const Mat& distCoeffs, const double xi, const vector<Vec3d>& rvecs, const vector<Vec3d>& tvecs,
+    vector<string> detec_list, const Mat& idx, const double rms, const vector<Mat>& imagePoints)
+{
+    FileStorage fs( filename, FileStorage::WRITE );
 
-  if(access(path_img.c_str(), R_OK ) == -1) {
-    ROS_ERROR("No such file: %s\nPlease give a path relative to catkin_ws/../captures/", path_img.c_str() );
-    return 1;
-  }
+    time_t tt;
+    time( &tt );
+    struct tm *t2 = localtime( &tt );
+    char buf[1024];
+    strftime( buf, sizeof(buf)-1, "%c", t2 );
 
-  ROS_INFO("calibration: using imagefile %s", path_img.c_str() );
-  ROS_INFO("calibration: using calibrationfile: %s", path_calib.c_str() );
+    fs << "calibration_time" << buf;
 
-  img    = cv::imread(path_img);
-  width  = img.cols;
-  height = img.rows;
-  cx     = width / 2;
-  cy     = height / 2;
+    if ( !rvecs.empty())
+        fs << "nFrames" << (int)rvecs.size();
 
+    if ( flags != 0)
+    {
+        sprintf( buf, "flags: %s%s%s%s%s%s%s%s%s",
+            flags & omnidir::CALIB_USE_GUESS ? "+use_intrinsic_guess" : "",
+            flags & omnidir::CALIB_FIX_SKEW ? "+fix_skew" : "",
+            flags & omnidir::CALIB_FIX_K1 ? "+fix_k1" : "",
+            flags & omnidir::CALIB_FIX_K2 ? "+fix_k2" : "",
+            flags & omnidir::CALIB_FIX_P1 ? "+fix_p1" : "",
+            flags & omnidir::CALIB_FIX_P2 ? "+fix_p2" : "",
+            flags & omnidir::CALIB_FIX_XI ? "+fix_xi" : "",
+            flags & omnidir::CALIB_FIX_GAMMA ? "+fix_gamma" : "",
+            flags & omnidir::CALIB_FIX_CENTER ? "+fix_center" : "");
+        //cvWriteComment( *fs, buf, 0 );
+    }
 
-  img.copyTo(img_ud);
-  cv::namedWindow("undistorted", CV_WINDOW_NORMAL);
-  cv::createTrackbar("focal length", "undistorted", &fl, fl_max, apply);
-  cv::createTrackbar("z distance", "undistorted", &wz, wzMax, apply);
+    fs << "flags" << flags;
 
-  cv::Mat* objMat = new cv::Mat(6,1,CV_64FC3);
-  objMat->at<cv::Point3f>(0,0) = cv::Point3f(915, 580, 0);
-  objMat->at<cv::Point3f>(1,0) = cv::Point3f(950, 560, 0);
-  objMat->at<cv::Point3f>(2,0) = cv::Point3f(920, 610, 0);
-  objMat->at<cv::Point3f>(3,0) = cv::Point3f(990, 600, 0);
-  objMat->at<cv::Point3f>(4,0) = cv::Point3f(910, 650, 0);
-  objMat->at<cv::Point3f>(5,0) = cv::Point3f(1015, 645, 0);
-  objectPoints.push_back(*objMat);
+    fs << "camera_matrix" << cameraMatrix;
+    fs << "distortion_coefficients" << distCoeffs;
+    fs << "xi" << xi;
 
-  cv::Mat* imgMat = new cv::Mat(6,1,CV_64FC2);
-  imgMat->at<cv::Point2f>(0,0) = cv::Point2f(235, 295);
-  imgMat->at<cv::Point2f>(1,0) = cv::Point2f(265, 360);
-  imgMat->at<cv::Point2f>(2,0) = cv::Point2f(215, 310);
-  imgMat->at<cv::Point2f>(3,0) = cv::Point2f(255, 370);
-  imgMat->at<cv::Point2f>(4,0) = cv::Point2f(200, 315);
-  imgMat->at<cv::Point2f>(5,0) = cv::Point2f(220, 385);
-  imagePoints.push_back(*imgMat);
+    //cvWriteComment( *fs, "names of images that are acturally used in calibration", 0 );
+    fs << "used_imgs" << "[";
+    for (int i = 0;  i < (int)idx.total(); ++i)
+    {
+        fs << detec_list[(int)idx.at<int>(i)];
+    }
+    fs << "]";
 
-  apply(0, (void*)0);
-  cv::waitKey(0);
+    if ( !rvecs.empty() && !tvecs.empty() )
+    {
+        Mat rvec_tvec((int)rvecs.size(), 6, CV_64F);
+        for (int i = 0; i < (int)rvecs.size(); ++i)
+        {
+            Mat(rvecs[i]).reshape(1, 1).copyTo(rvec_tvec(Rect(0, i, 3, 1)));
+            Mat(tvecs[i]).reshape(1, 1).copyTo(rvec_tvec(Rect(3, i, 3, 1)));
+        }
+        //cvWriteComment( *fs, "a set of 6-tuples (rotation vector + translation vector) for each view", 0 );
+        fs << "extrinsic_parameters" << rvec_tvec;
+    }
 
-  std::ofstream file;
-  file.open(path_calib.c_str(), std::ios::out | std::ios::trunc);
+    fs << "rms" << rms;
 
-  file << width << " " << height << " " << cx << " " << cy << " "
-      << fl << " " << (ceil_height / 100.0) << " " << (scale / 10.0) << std::endl;
+    if ( !imagePoints.empty() )
+    {
+        Mat imageMat((int)imagePoints.size(), (int)imagePoints[0].total(), CV_64FC2);
+        for (int i = 0; i < (int)imagePoints.size(); ++i)
+        {
+            Mat r = imageMat.row(i).reshape(2, imageMat.cols);
+            Mat imagei(imagePoints[i]);
+            imagei.copyTo(r);
+        }
+        fs << "image_points" << imageMat;
+    }
+}
 
-  file.close();
-  ROS_INFO("Calibration written to %s.", path_calib.c_str() );
+int main(int argc, char** argv)
+{
+    cv::CommandLineParser parser(argc, argv,
+                                 "{w||board width}"
+                                 "{h||board height}"
+                                 "{sw|1.0|square width}"
+                                 "{sh|1.0|square height}"
+                                 "{o|out_camera_params.xml|output file}"
+                                 "{fs|false|fix skew}"
+                                 "{fp|false|fix principal point at the center}"
+                                 "{@input||input file - xml file with a list of the images, created with cpp-example-imagelist_creator tool}"
+                                 "{help||show help}"
+                                 );
+    parser.about("This is a sample for omnidirectional camera calibration. Example command line:\n"
+                 "    omni_calibration -w=6 -h=9 -sw=80 -sh=80 imagelist.xml \n");
+    if (parser.has("help") || !parser.has("w") || !parser.has("h"))
+    {
+        parser.printMessage();
+        return 0;
+    }
 
-  return 0;
+    Size boardSize(parser.get<int>("w"), parser.get<int>("h"));
+    Size2d squareSize(parser.get<double>("sw"), parser.get<double>("sh"));
+    int flags = 0;
+    if (parser.get<bool>("fs"))
+        flags |= omnidir::CALIB_FIX_SKEW;
+    if (parser.get<bool>("fp"))
+        flags |= omnidir::CALIB_FIX_CENTER;
+    const string outputFilename = parser.get<string>("o");
+    const string inputFilename = parser.get<string>(0);
+
+    if (!parser.check())
+    {
+        parser.printErrors();
+        return -1;
+    }
+
+    // get image name list
+    vector<string> image_list, detec_list;
+    if(!readStringList(inputFilename, image_list))
+    {
+        cout << "Can not read imagelist" << endl;
+        return -1;
+    }
+
+    // find corners in images
+    // some images may be failed in automatic corner detection, passed cases are in detec_list
+    cout << "Detecting chessboards (" << image_list.size() << ")" << endl;
+    vector<Mat> imagePoints;
+    Size imageSize;
+    if(!detecChessboardCorners(image_list, detec_list, imagePoints, boardSize, imageSize))
+    {
+        cout << "Not enough corner detected images" << endl;
+        return -1;
+    }
+
+    // calculate object coordinates
+    vector<Mat> objectPoints;
+    Mat object;
+    calcChessboardCorners(boardSize, squareSize, object);
+    for(int i = 0; i < (int)detec_list.size(); ++i)
+        objectPoints.push_back(object);
+
+    // run calibration, some images are discarded in calibration process because they are failed
+    // in initialization. Retained image indexes are in idx variable.
+    Mat K, D, xi, idx;
+    vector<Vec3d> rvecs, tvecs;
+    double _xi, rms;
+    TermCriteria criteria(3, 200, 1e-8);
+    rms = omnidir::calibrate(objectPoints, imagePoints, imageSize, K, xi, D, rvecs, tvecs, flags, criteria, idx);
+    _xi = xi.at<double>(0);
+    cout << "Saving camera params to " << outputFilename << endl;
+    saveCameraParams(outputFilename, flags, K, D, _xi,
+        rvecs, tvecs, detec_list, idx, rms, imagePoints);
 }
