784ffd59601bc85b88703e557fe2b03356815b8a
SH <s.heinrichsen@fu-berlin.de>
2018-08-13 18:08:11 +0200

    live calibration works!

--- catkin_ws_user/src/fisheye_camera_matrix/src/offline_calibration_manual.cpp
+++ catkin_ws_user/src/fisheye_camera_matrix/src/offline_calibration_manual.cpp
@@ -19,6 +19,198 @@
 #include "sensor_msgs/image_encodings.h"
 
 
+class CCalibrateOmniCamera
+{
+private:
+
+	ros::NodeHandle m_oNodeHandle;
+	image_transport::ImageTransport m_oImgTransport;
+	image_transport::Subscriber m_oImageSub;
+
+	const std::string m_sWindowName = "Calibration";
+
+	std::string								m_sOutputFilename;
+	std::vector<std::vector<cv::Point2f>> 	m_aImagePoints;
+	std::vector<std::vector<cv::Point3f>> 	m_aObjectPoints;
+	cv::Size								m_oBoardSize;
+	cv::Size								m_oImageSize;
+
+	bool*	m_pButtonClicked;
+
+public:
+
+	CCalibrateOmniCamera(std::string sOutputFilename, std::vector<std::vector<cv::Point3f>>& aObjectPoints, cv::Size& oBoardSize,
+			std::string sImageTopic)
+	: m_oImgTransport(m_oNodeHandle), m_pButtonClicked(new bool(false))
+	{
+		m_sOutputFilename	= sOutputFilename;
+		m_aObjectPoints 	= aObjectPoints;
+		m_oBoardSize 		= oBoardSize;
+
+		cv::namedWindow(m_sWindowName, cv::WINDOW_GUI_EXPANDED);
+		cv::createButton("Calibrate now.", CCalibrateOmniCamera::CalibrateButton, m_pButtonClicked, cv::QT_PUSH_BUTTON | cv::QT_NEW_BUTTONBAR);
+		cv::displayStatusBar(m_sWindowName, "0 valid frames received.");
+
+		m_oImageSub = m_oImgTransport.subscribe(sImageTopic, 1,
+				  &CCalibrateOmniCamera::ImageCallback, this, image_transport::TransportHints("compressed"));
+	}
+
+	~CCalibrateOmniCamera()
+	{
+		m_oImageSub.shutdown();
+		cv::destroyWindow(m_sWindowName);
+		delete m_pButtonClicked;
+	}
+
+private:
+
+	void ImageCallback(const sensor_msgs::ImageConstPtr& msg)
+	{
+		if (*m_pButtonClicked)
+		{
+			CalibrateAndSave();
+			return;
+		}
+
+		cv_bridge::CvImagePtr pCvImg;
+		try
+		{
+		  pCvImg = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
+		}
+		catch (cv_bridge::Exception& e)
+		{
+		  ROS_ERROR("cv_bridge exception: %s", e.what());
+		  return;
+		}
+		cv::Mat oGrayImg;
+		cv::cvtColor(pCvImg->image, oGrayImg, cv::COLOR_BGR2GRAY);
+
+        std::vector<cv::Point2f> aPoints;
+        bool found = findCirclesGrid(oGrayImg, m_oBoardSize, aPoints, cv::CALIB_CB_ASYMMETRIC_GRID);
+        if (found)
+        {
+            m_aImagePoints.push_back(aPoints);
+            std::ostringstream strs;
+            strs << m_aImagePoints.size() << " valid frames received.";
+    		cv::displayStatusBar(m_sWindowName, strs.str());
+        }
+
+        m_oImageSize = oGrayImg.size();
+
+        cv::imshow(m_sWindowName, pCvImg->image);
+        cv::waitKey(1);
+	}
+
+	void CalibrateAndSave()
+	{
+		*m_pButtonClicked = false;
+
+		if (m_aImagePoints.size() < 3)
+		{
+	        std::cout << "Not enough corner detected images" << std::endl;
+	        return;
+		}
+
+		m_aObjectPoints.resize(m_aImagePoints.size(), m_aObjectPoints[0]);
+
+		// run calibration, some images are discarded in calibration process because they are failed
+		// in initialization. Retained image indexes are in idx variable.
+		int flags = 0;
+		cv::Mat K, D, xi, idx;
+		std::vector<cv::Vec3d> rvecs, tvecs;
+		double _xi, rms;
+		cv::TermCriteria criteria(3, 200, 1e-8);
+		rms = cv::omnidir::calibrate(m_aObjectPoints, m_aImagePoints, m_oImageSize, K, xi, D, rvecs, tvecs, flags, criteria, idx);
+		std::cout << "rms: " << rms << std::endl;
+		_xi = xi.at<double>(0);
+		std::cout << "Saving camera params to " << m_sOutputFilename << std::endl;
+		saveCameraParams(m_sOutputFilename, flags, K, D, _xi,
+			rvecs, tvecs, idx, rms, m_aImagePoints);
+
+		std::ostringstream strs;
+		strs << "calibration error: " << rms;
+		cv::displayOverlay(m_sWindowName, strs.str(), 5000);
+	}
+
+	/**
+	 * no idea why this has to be static, bad design IMO.
+	 * This is obviously the Callback for pressing the Calibrate Button
+	 * @param state			The state of the button. Apparently always -1 for a QPushButton
+	 * @param userData		The userData. In this case a pointer to a bool value.
+	 */
+	static void CalibrateButton(int state, void* userData)
+	{
+		bool* bButtonClicked = (bool*)userData;
+		*bButtonClicked = true;
+	}
+
+	static void saveCameraParams( const std::string & filename, int flags, const cv::Mat& cameraMatrix,
+	    const cv::Mat& distCoeffs, const double xi, const std::vector<cv::Vec3d>& rvecs, const std::vector<cv::Vec3d>& tvecs,
+	    const cv::Mat& idx, const double rms, const std::vector<std::vector<cv::Point2f>>& imagePoints)
+	{
+	    cv::FileStorage fs( filename, cv::FileStorage::WRITE );
+
+	    time_t tt;
+	    time( &tt );
+	    struct tm *t2 = localtime( &tt );
+	    char buf[1024];
+	    strftime( buf, sizeof(buf)-1, "%c", t2 );
+
+	    fs << "calibration_time" << buf;
+
+	    if ( !rvecs.empty())
+	        fs << "nFrames" << (int)rvecs.size();
+
+	    if ( flags != 0)
+	    {
+	        sprintf( buf, "flags: %s%s%s%s%s%s%s%s%s",
+	            flags & cv::omnidir::CALIB_USE_GUESS ? "+use_intrinsic_guess" : "",
+	            flags & cv::omnidir::CALIB_FIX_SKEW ? "+fix_skew" : "",
+	            flags & cv::omnidir::CALIB_FIX_K1 ? "+fix_k1" : "",
+	            flags & cv::omnidir::CALIB_FIX_K2 ? "+fix_k2" : "",
+	            flags & cv::omnidir::CALIB_FIX_P1 ? "+fix_p1" : "",
+	            flags & cv::omnidir::CALIB_FIX_P2 ? "+fix_p2" : "",
+	            flags & cv::omnidir::CALIB_FIX_XI ? "+fix_xi" : "",
+	            flags & cv::omnidir::CALIB_FIX_GAMMA ? "+fix_gamma" : "",
+	            flags & cv::omnidir::CALIB_FIX_CENTER ? "+fix_center" : "");
+	        //cvWriteComment( *fs, buf, 0 );
+	    }
+
+	    fs << "flags" << flags;
+
+	    fs << "camera_matrix" << cameraMatrix;
+	    fs << "distortion_coefficients" << distCoeffs;
+	    fs << "xi" << xi;
+
+	    if ( !rvecs.empty() && !tvecs.empty() )
+	    {
+	        cv::Mat rvec_tvec((int)rvecs.size(), 6, CV_64F);
+	        for (int i = 0; i < (int)rvecs.size(); ++i)
+	        {
+	            cv::Mat(rvecs[i]).reshape(1, 1).copyTo(rvec_tvec(cv::Rect(0, i, 3, 1)));
+	            cv::Mat(tvecs[i]).reshape(1, 1).copyTo(rvec_tvec(cv::Rect(3, i, 3, 1)));
+	        }
+	        //cvWriteComment( *fs, "a set of 6-tuples (rotation vector + translation vector) for each view", 0 );
+	        fs << "extrinsic_parameters" << rvec_tvec;
+	    }
+
+	    fs << "rms" << rms;
+
+	    if ( !imagePoints.empty() )
+	    {
+	        cv::Mat imageMat((int)imagePoints.size(), (int)imagePoints[0].size(), CV_64FC2);
+	        for (int i = 0; i < (int)imagePoints.size(); ++i)
+	        {
+	            cv::Mat r = imageMat.row(i).reshape(2, imageMat.cols);
+	            cv::Mat imagei(imagePoints[i]);
+	            imagei.copyTo(r);
+	        }
+	        fs << "image_points" << imageMat;
+	    }
+	}
+
+};
+
 /**
  *
  * @param boardSize			size of the async circle grid
@@ -40,162 +232,31 @@ static void calcAsyncCircleCorners(const cv::Size &boardSize, const cv::Size2f &
     }
 }
 
-
-static bool detecAsincCircleCorners(const std::vector<std::string>& list, std::vector<std::string>& list_detected,
-    std::vector<std::vector<cv::Point2f>>& imagePoints, cv::Size boardSize, cv::Size& imageSize)
-{
-    imagePoints.resize(0);
-    list_detected.resize(0);
-    int n_img = (int)list.size();
-    cv::Mat img;
-    for(int i = 0; i < n_img; ++i)
-    {
-        std::cout << list[i] << "... ";
-        std::vector<cv::Point2f> points;
-        img = imread(list[i], cv::IMREAD_GRAYSCALE);
-        bool found = findCirclesGrid(img, boardSize, points, cv::CALIB_CB_ASYMMETRIC_GRID);
-        if (found)
-        {
-            imagePoints.push_back(points);
-            list_detected.push_back(list[i]);
-        }
-        std::cout << (found ? "FOUND" : "NO") << std::endl;
-    }
-    if (!img.empty())
-        imageSize = img.size();
-    if (imagePoints.size() < 3)
-        return false;
-    else
-        return true;
-}
-
-static bool readStringList( const std::string& filename, std::vector<std::string>& l )
-{
-    std::ifstream infile(filename);
-    std::string line;
-    while (std::getline(infile, line))
-    {
-    	l.push_back(line);
-    }
-
-    return true;
-}
-
-static void saveCameraParams( const std::string & filename, int flags, const cv::Mat& cameraMatrix,
-    const cv::Mat& distCoeffs, const double xi, const std::vector<cv::Vec3d>& rvecs, const std::vector<cv::Vec3d>& tvecs,
-    std::vector<std::string> detec_list, const cv::Mat& idx, const double rms, const std::vector<std::vector<cv::Point2f>>& imagePoints)
-{
-	// show undistorted images
-	for (std::string sImage : detec_list)
-	{
-		cv::namedWindow(sImage, CV_WINDOW_NORMAL);
-		cv::Mat distorted = cv::imread(sImage);
-		cv::Mat undistorted;
-		cv::omnidir::undistortImage(distorted, undistorted, cameraMatrix, distCoeffs, xi, cv::omnidir::RECTIFY_PERSPECTIVE);
-		imshow(sImage, undistorted);
-	}
-	cv::waitKey(0);
-
-
-    cv::FileStorage fs( filename, cv::FileStorage::WRITE );
-
-    time_t tt;
-    time( &tt );
-    struct tm *t2 = localtime( &tt );
-    char buf[1024];
-    strftime( buf, sizeof(buf)-1, "%c", t2 );
-
-    fs << "calibration_time" << buf;
-
-    if ( !rvecs.empty())
-        fs << "nFrames" << (int)rvecs.size();
-
-    if ( flags != 0)
-    {
-        sprintf( buf, "flags: %s%s%s%s%s%s%s%s%s",
-            flags & cv::omnidir::CALIB_USE_GUESS ? "+use_intrinsic_guess" : "",
-            flags & cv::omnidir::CALIB_FIX_SKEW ? "+fix_skew" : "",
-            flags & cv::omnidir::CALIB_FIX_K1 ? "+fix_k1" : "",
-            flags & cv::omnidir::CALIB_FIX_K2 ? "+fix_k2" : "",
-            flags & cv::omnidir::CALIB_FIX_P1 ? "+fix_p1" : "",
-            flags & cv::omnidir::CALIB_FIX_P2 ? "+fix_p2" : "",
-            flags & cv::omnidir::CALIB_FIX_XI ? "+fix_xi" : "",
-            flags & cv::omnidir::CALIB_FIX_GAMMA ? "+fix_gamma" : "",
-            flags & cv::omnidir::CALIB_FIX_CENTER ? "+fix_center" : "");
-        //cvWriteComment( *fs, buf, 0 );
-    }
-
-    fs << "flags" << flags;
-
-    fs << "camera_matrix" << cameraMatrix;
-    fs << "distortion_coefficients" << distCoeffs;
-    fs << "xi" << xi;
-
-    //cvWriteComment( *fs, "names of images that are acturally used in calibration", 0 );
-    fs << "used_imgs" << "[";
-    for (int i = 0;  i < (int)idx.total(); ++i)
-    {
-        fs << detec_list[(int)idx.at<int>(i)];
-    }
-    fs << "]";
-
-    if ( !rvecs.empty() && !tvecs.empty() )
-    {
-        cv::Mat rvec_tvec((int)rvecs.size(), 6, CV_64F);
-        for (int i = 0; i < (int)rvecs.size(); ++i)
-        {
-            cv::Mat(rvecs[i]).reshape(1, 1).copyTo(rvec_tvec(cv::Rect(0, i, 3, 1)));
-            cv::Mat(tvecs[i]).reshape(1, 1).copyTo(rvec_tvec(cv::Rect(3, i, 3, 1)));
-        }
-        //cvWriteComment( *fs, "a set of 6-tuples (rotation vector + translation vector) for each view", 0 );
-        fs << "extrinsic_parameters" << rvec_tvec;
-    }
-
-    fs << "rms" << rms;
-
-    if ( !imagePoints.empty() )
-    {
-        cv::Mat imageMat((int)imagePoints.size(), (int)imagePoints[0].size(), CV_64FC2);
-        for (int i = 0; i < (int)imagePoints.size(); ++i)
-        {
-            cv::Mat r = imageMat.row(i).reshape(2, imageMat.cols);
-            cv::Mat imagei(imagePoints[i]);
-            imagei.copyTo(r);
-        }
-        fs << "image_points" << imageMat;
-    }
-}
-
 int main(int argc, char** argv)
 {
+	// parse command line arguments
     cv::CommandLineParser parser(argc, argv,
                                  "{w|4|board width}"
                                  "{h|11|board height}"
                                  "{sw|0.06|square width}"
                                  "{sh|0.06|square height}"
                                  "{o|out_camera_params.xml|output file}"
-                                 "{fs|false|fix skew}"
-                                 "{fp|false|fix principal point at the center}"
-                                 "{@input||input file - txt file with a list of the images}"
+    							 "{t|/usb_cam/image_raw|ros image topic}"
                                  "{help||show help}"
                                  );
     parser.about("This is a sample for omnidirectional camera calibration. Example command line:\n"
-                 "    omni_calibration -w=6 -h=9 -sw=80 -sh=80 imagelist.xml \n");//
+                 "    omni_calibration -w=6 -h=9 -sw=80 -sh=80 \n");//
     if (parser.has("help"))
     {
         parser.printMessage();
         return 0;
     }
 
+    std::string sTopicName = parser.get<std::string>("t");
+
     cv::Size boardSize(parser.get<int>("w"), parser.get<int>("h"));
     cv::Size2f squareSize(parser.get<float>("sw"), parser.get<float>("sh"));
-    int flags = 0;
-    if (parser.get<bool>("fs"))
-        flags |= cv::omnidir::CALIB_FIX_SKEW;
-    if (parser.get<bool>("fp"))
-        flags |= cv::omnidir::CALIB_FIX_CENTER;
     const std::string outputFilename = parser.get<std::string>("o");
-    const std::string inputFilename = parser.get<std::string>(0);
 
     if (!parser.check())
     {
@@ -203,44 +264,14 @@ int main(int argc, char** argv)
         return -1;
     }
 
-    // get image name list
-    std::vector<std::string> image_list, detec_list;
-    if(!readStringList(inputFilename, image_list))
-    {
-        std::cout << "Can not read imagelist" << std::endl;
-        return -1;
-    }
-
-    // find corners in images
-    // some images may be failed in automatic corner detection, passed cases are in detec_list
-    std::cout << "Detecting chessboards (" << image_list.size() << ")" << std::endl;
-    std::vector<std::vector<cv::Point2f>> imagePoints;
-    cv::Size imageSize;
-    if(!detecAsincCircleCorners(image_list, detec_list, imagePoints, boardSize, imageSize))
-    {
-        std::cout << "Not enough corner detected images" << std::endl;
-        return -1;
-    }
-
     // calculate object coordinates
     std::vector<std::vector<cv::Point3f>> objectPoints(1);
     calcAsyncCircleCorners(boardSize, squareSize, objectPoints[0]);
-    objectPoints.resize(imagePoints.size(), objectPoints[0]);
 
-    // run calibration, some images are discarded in calibration process because they are failed
-    // in initialization. Retained image indexes are in idx variable.
-    cv::Mat K, D, xi, idx;
-    std::vector<cv::Vec3d> rvecs, tvecs;
-    double _xi, rms;
-    cv::TermCriteria criteria(3, 200, 1e-8);
-    std::cout << imagePoints.size() << " " << objectPoints.size() << " " << imageSize << std::endl;
-//    cv::Mat camMat, coeffs;
-//    rms = calibrateCamera(objectPoints, imagePoints, imageSize, camMat, coeffs, rvecs, tvecs);
-    rms = cv::omnidir::calibrate(objectPoints, imagePoints, imageSize, K, xi, D, rvecs, tvecs, flags, criteria, idx);
-    std::cout << "rms: " << rms << std::endl;
-    _xi = xi.at<double>(0);
-    std::cout << "Saving camera params to " << outputFilename << std::endl;
-    saveCameraParams(outputFilename, flags, K, D, _xi,
-        rvecs, tvecs, detec_list, idx, rms, imagePoints);
+    // start the live calibration
+	ros::init(argc, argv, "live_calibration");
+    CCalibrateOmniCamera oCalibrate(outputFilename, objectPoints, boardSize, sTopicName);
+    ros::spin();
+    return 0;
 
 }
